7888f58cd83df7f31843e41bfc82224163d87fcc
George Straehle <gstraehle@gmail.com>
2016-01-24 13:03:01 -0500

    added acces to gallery-db

--- application.js
+++ application.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
 
   $('.gallery').click(GalleryApp.toggleGallery);
   $('.doneviewing').click(function(){$('.livegallery').hide('slow'); $('.gallery').show();});
-
   lightbox.option({
     'resizeDuration': 100,
     'wrapAround': true
   });
+  jQuery.get('http://gstraehle.github.io/gallery/gallery-db.csv', function(data) {DB = $.csv.toArrays(data);});
 });
 
 var GalleryApp = GalleryApp || {};
+var DB = [];
 
 GalleryApp.toggleGallery = function(){
   var activeGallery = $(this).attr('id');
@@ -25,3 +26,31 @@ GalleryApp.toggleGallery = function(){
 
     ga('create', 'UA-72832610-1', 'auto');
     ga('send', 'pageview');
+
+// function CSVToArray( strData ){
+//   var strDelimiter =  ",";
+//   var objPattern = new RegExp(
+//       ("(\\" + strDelimiter + "|\\r?\\n|\\r|^)" + "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" + "([^\"\\" + strDelimiter + "\\r\\n]*))"
+//       ),"gi");
+//   var arrData = [[]];
+//   var arrMatches = null;
+//   while (arrMatches = objPattern.exec( strData )){
+//     var strMatchedDelimiter = arrMatches[ 1 ];
+//     if (
+//         strMatchedDelimiter.length &&
+//         strMatchedDelimiter !== strDelimiter
+//         ){
+//         arrData.push( [] );
+//       }
+//       var strMatchedValue;
+//       if (arrMatches[ 2 ]){
+//           strMatchedValue = arrMatches[ 2 ].replace(
+//               new RegExp( "\"\"", "g" ),"\"");
+//       } else {
+//           strMatchedValue = arrMatches[ 3 ];
+//       }
+//       arrData[ arrData.length - 1 ].push( strMatchedValue );
+//   }
+//   return( arrData );
+//   console.log(arrData);
+// }
