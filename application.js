8807cd44bce5b10835c2daaa14dfc56b8626e882
George Straehle <gstraehle@gmail.com>
2016-01-24 22:55:41 -0500

    fixed lightbox title rendering and added logic to remove spans.  had to move the spand details over as they were finding the next div rather than their own

--- application.js
+++ application.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.doneviewing').click(function(){
-    $('.livegallery a').remove();
-    $('.livegallery h3').remove();
+    $('.livegallery a, .livegallery h3,.livegallery span').remove();
     $('.livegallery').hide('slow');
     $('.gallerysummary').show();
   });
@@ -25,7 +24,7 @@ GalleryApp.getGallery = function(){
   var galleryID = $(this).attr('id');
   jQuery.each(DB,function(){
     if($(this)[2] === galleryID){
-      $(".livegallery").append("<a href='" + $(this)[0] + "' data-lightbox='gallery-1'><img class='thumbnails' title='" + $(this)[4] + "' src='" + $(this)[1] + "' alt=''/></a><span class='spantitle'>" + $(this)[3] + "</span>");
+      $(".livegallery").append("<span class='spantitle'>" + $(this)[3] + "</span><a href='" + $(this)[0] + "' data-lightbox='gallery-1' data-title='"+$(this)[3]+"'><img class='thumbnails' title='" + $(this)[4] + "' src='" + $(this)[1] + "' alt=''/></a>" );
     }
   });
 };
