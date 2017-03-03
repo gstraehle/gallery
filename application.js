32a56a1ec609bdf37e667ccc0f4915ac10bcaa11
George Straehle <gstraehle@gmail.com>
2017-03-03 01:41:58 -0500

    updated centering and footer.  tried keeping footer at bottom.. but impossible

--- application.js
+++ application.js
@@ -6,17 +6,12 @@ $(document).ready(function() {
   });
   jQuery.get('http://gstraehle.github.io/gallery/gallery-db.csv', function(data) {DB = $.csv.toArrays(data);});
   $('.gallery').click(GalleryApp.getGallery);
-  // $('.lb-close').click(function(){console.log('close gallery');});
   $('.eventdetailcue').click(function(){$('.eventdetail, .eventsummary').toggle("slow");});
 });
 
 var GalleryApp = GalleryApp || {};
 var DB = [];
 var yposition = 0;
-
-// window.onload = function(){
-//   $('head').append("<link rel='shortcut icon' href='https://s3.amazonaws.com/gms-gallery/favicon.ico' type='image/x-icon'><link rel='icon' href='https://s3.amazonaws.com/gms-gallery/favicon.ico' type='image/x-icon'>");
-// };
 
 GalleryApp.doneViewing = function(){
   $('.thumbnaildivs, .livegallery h3').remove();
@@ -28,14 +23,13 @@ GalleryApp.doneViewing = function(){
 
 GalleryApp.getGallery = function(){
   yposition = $(this).offset().top - 100;
-  $('.topbar').css('text-align', 'left');
+  var galleryID = $(this).attr('id');
   var galleryTitle = $(this).data('title');
-  var galleryDescription = $(this).data('description');
+  $('.topbar').css('text-align', 'left');
   $('.gallerysummary').hide();
   $('.livegallery').show();
   $('body').scrollTop(0);
   $('.livegallery').append("<h3>" + galleryTitle + "</h3");
-  var galleryID = $(this).attr('id');
   jQuery.each(DB,function(){
     if($(this)[2] === galleryID){
       $(".livegallery").append("<div class='thumbnaildivs'><a href='" + $(this)[0] + "' data-lightbox='gallery-1' data-title='" + $(this)[3]+ "<br><i>" + $(this)[4] +"</i>" +"'><span class='spantitle'>" + $(this)[3] + "</span><img class='thumbnails' title='" + $(this)[4] + "' src='" + $(this)[1] + "' alt=''/></a></div>" );
